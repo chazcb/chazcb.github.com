26e5e677b32bef8ef2e79dfcd7d1dd8c5348b593
Charles Covey-Brandt <chazcb@gmail.com>
2014-03-15 13:30:02 -0700

    Code eval lowercase.

---
+++ codeeval/lower.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    var fs  = require('fs');
+
+    fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+        if (line !== '') {
+            console.log(line.toLowerCase());
+        }
+    });
+})();
