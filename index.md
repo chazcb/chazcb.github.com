93228899babd13c2022037cc843ce91837f9333a
Charles Covey-Brandt <chazcb@gmail.com>
2015-07-05 06:49:31 -0700

    Reveal slideshow site up and running.

--- index.md
+++ index.md
@@ -1,46 +1,47 @@
 ---
-layout: page
-title: Hello World!
-tagline: Supporting tagline
+layout: slides
 ---
-{% include JB/setup %}
 
-Read [Jekyll Quick Start](http://jekyllbootstrap.com/usage/jekyll-quick-start.html)
+# Hi, I'm Charles
 
-Complete usage and documentation available at: [Jekyll Bootstrap](http://jekyllbootstrap.com)
+# I engineer software
 
-## Update Author Attributes
+# And develop teams and people
 
-In `_config.yml` remember to specify your own data:
-    
-    title : My Blog =)
-    
-    author :
-      name : Name Lastname
-      email : blah@email.test
-      github : username
-      twitter : username
+-----
 
-The theme should reference these variables whenever needed.
-    
-## Sample Posts
+## *I believe in these people things*
 
-This blog contains sample posts which help stage pages and blog data.
-When you don't need the samples anymore just delete the `_posts/core-samples` folder.
+- Servant leadership,
+- Cultural diversity,
+- Better hiring processes,
+- Training and mentorship;
 
-    $ rm -rf _posts/core-samples
+-----
 
-Here's a sample "posts list".
+### *and these technical things.*
+- Coherent code,
+- Tested code,
+- Front-end frameworks,
+- RESTful APIs,
+- Service Oriented Architecture,
 
-<ul class="posts">
-  {% for post in site.posts %}
-    <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ BASE_PATH }}{{ post.url }}">{{ post.title }}</a></li>
-  {% endfor %}
-</ul>
+-----
 
-## To-Do
+#### *Some stuff I use a lot*
+- JavaScript
+- Python
+- Backbone.js / Marionette / Handlebars, etc.
+- Django
 
-This theme is still unfinished. If you'd like to be added as a contributor, [please fork](http://github.com/plusjade/jekyll-bootstrap)!
-We need to clean up the themes, make theme usage guides with theme-specific markup examples.
+-----
 
+#### *and some other stuff I have experience with.*
+- CoffeeScript
+- Groovy
+- Git
+- Continuous deployment
+- The Web
+- Architecting for scale with SQL || flat stores like C\*
+- `{React|Angular|Meteor|Ember}.js`
 
---
+++ js/slides.js
@@ -0,0 +1,59 @@
+/* global Reveal */
+(function () {
+    'use strict';
+
+    // Convert HR separated markdown output to individual
+    // slides for Reveal.js.
+    (function () {
+        var wrapper = document.getElementById('slides');
+        var fragment = document.createDocumentFragment();
+
+        // Loop through each of the elements in the
+        // wrapper and group them by the HR separater.
+        var slide = document.createElement('section');
+        var children = wrapper.childNodes;
+        children = Array.prototype.slice.call(children);
+        children.forEach(function (el) {
+            if (el.tagName === 'HR') {
+                fragment.appendChild(slide);
+                slide = document.createElement('section');
+            } else {
+                slide.appendChild(el);
+            }
+        });
+
+        // Cleanup anything left over by the for loop.
+        fragment.appendChild(slide);
+
+        // Remove all els from the wrapper if there are
+        // any left.
+        while (wrapper.firstChild)
+            wrapper.removeChild(wrapper.firstChild);
+
+        // Append the slideshow fragment to the wrapper.
+        wrapper.appendChild(fragment);
+    })();
+
+    // Full list of configuration options available at:
+    // https://github.com/hakimel/reveal.js#configuration
+
+    Reveal.initialize({
+        // controls: true,
+        progress: true,
+        history: true,
+        center: true,
+        mouseWheel: true,
+
+        transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+        // Optional reveal.js plugins
+        dependencies: [
+            { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+            // { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            // { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            // { src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+            { src: 'plugin/zoom-js/zoom.js', async: true },
+            { src: 'plugin/notes/notes.js', async: true }
+        ]
+    });
+})();
