31f907caff5a222ba00f435c7d8cd524428ef11b
Charles Covey-Brandt <chazcb@gmail.com>
2015-10-11 19:04:09 -0700

    Update for H1 H2 subsection slides, will break most of my MD files, though.

--- js/slides.js
+++ js/slides.js
@@ -2,28 +2,37 @@
 (function () {
     'use strict';
 
-    // Convert HR separated markdown output to individual
-    // slides for Reveal.js.
+    // Convert H1 & H2 separated markdown output to slides and sub-slides for Reveal.js.
     (function () {
         var wrapper = document.getElementById('slides');
-        var fragment = document.createDocumentFragment();
 
         // Loop through each of the elements in the
         // wrapper and group them by the HR separater.
-        var slide = document.createElement('section');
-        var children = wrapper.childNodes;
-        children = Array.prototype.slice.call(children);
-        children.forEach(function (el) {
-            if (el.tagName === 'HR') {
-                fragment.appendChild(slide);
-                slide = document.createElement('section');
-            } else {
-                slide.appendChild(el);
-            }
-        });
+        var children = Array.prototype.slice.call(wrapper.childNodes);
 
-        // Cleanup anything left over by the for loop.
-        fragment.appendChild(slide);
+        var fragment = document.createDocumentFragment();
+        var section = null;
+        var subsection = null;
+
+        children.forEach(function (el, index) {
+
+            if (el.tagName === 'H1') {
+                // create a new top level slide and sub-section
+                // and attach to the wrapper.
+                section = document.createElement('section');
+                subsection = document.createElement('section');
+                section.appendChild(subsection);
+                fragment.appendChild(section);
+            } else if (el.tagName === 'H2') {
+                // just create a new section for the current
+                // slide
+                subsection = document.createElement('section')
+                section.appendChild(subsection);
+            }
+
+            if (subsection)
+                subsection.appendChild(el);
+        });
 
         // Remove all els from the wrapper if there are
         // any left.
